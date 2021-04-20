9f4d2cf976bff4254ffa8714927a3c8d5caba4ff
Raffi <raffij@gmail.com>
2021-04-20 11:53:38 +0100

    Add build files

---
+++ dist/stylis-plugin-extra-scope.js
@@ -0,0 +1,45 @@
+'use strict'
+
+Object.defineProperty(exports, '__esModule', { value: true })
+
+function createExtraScopePlugin() {
+  for (
+    var _len = arguments.length, scopes = new Array(_len), _key = 0;
+    _key < _len;
+    _key++
+  ) {
+    scopes[_key] = arguments[_key]
+  }
+
+  scopes = scopes.map(function(scope) {
+    return scope.trim() + ' '
+  })
+  return function(element) {
+    var _element$root
+
+    if (element.type !== 'rule') {
+      return
+    }
+
+    if (
+      ((_element$root = element.root) == null ? void 0 : _element$root.type) ===
+      '@keyframes'
+    ) {
+      return
+    }
+
+    if (
+      !element.parent ||
+      (element.props.length === 1 && element.value.charCodeAt(0) !== 58) ||
+      !element.length
+    ) {
+      element.props = element.props.flatMap(function(prop) {
+        return scopes.map(function(scope) {
+          return scope + prop
+        })
+      })
+    }
+  }
+}
+
+exports.default = createExtraScopePlugin
